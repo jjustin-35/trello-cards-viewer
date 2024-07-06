6b964efce053856f5be886882ebdfce27e0f09d1
jjustin-chen <justin.chen@kdanmobile.com>
2024-07-07 01:02:43 +0800

    update: add server worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,54 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const { DefinePlugin } = require('webpack');
-const dotenv = require('dotenv');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: {
+    main: "./src/index.tsx",
+    worker: "./src/server-worker.ts",
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: (pathData) =>
+      pathData.chunk.name === "worker" ? "server-worker.js" : "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
     new CopyPlugin({
-      patterns: [
-        { from: 'src/manifest.json', to: 'manifest.json' },
-      ],
+      patterns: [{ from: "src/manifest.json", to: "manifest.json" }],
     }),
     new DefinePlugin({
-      'process.env.TRELLO_API_KEY': JSON.stringify(process.env.TRELLO_API_KEY),
-      'process.env.TRELLO_API_TOKEN': JSON.stringify(process.env.TRELLO_API_TOKEN),
+      "process.env.TRELLO_API_KEY": JSON.stringify(process.env.TRELLO_API_KEY),
+      "process.env.TRELLO_API_TOKEN": JSON.stringify(
+        process.env.TRELLO_API_TOKEN
+      ),
     }),
   ],
   devServer: {
-    port: '3000',
-    static: ['./dist'],
+    port: "3000",
+    static: ["./dist"],
     open: true,
     hot: true,
     liveReload: true,
